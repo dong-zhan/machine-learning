d4ae77d859adebad7bb1e698c25120aff32adffd
dong-zhan <41976575+dong-zhan@users.noreply.github.com>
2020-08-12 19:26:12 -0700

    Create simplest model.py

---
+++ keras/simplest model.py
@@ -0,0 +1,59 @@
+def create_nw():
+	from keras.optimizers import SGD
+
+	global model, functors, func, layer
+	model = tf.keras.models.Sequential()
+	model.add(tf.keras.Input(shape=(1,)))							#input layer
+	model.add(tf.keras.layers.Dense(1, activation='relu'))   		#output layer
+	
+	#opt = SGD(lr=0.01)
+	model.compile(loss='MeanSquaredError', optimizer=keras.optimizers.Adam(lr=0.001), metrics=['accuracy'])  #MeanSquaredError binary_crossentropy
+	
+	model.summary()
+	print("learning rate:", K.eval(model.optimizer.lr))
+	
+	layer = model.layers[0]
+
+	func = K.function([model.input], [model.layers[0].output])		#func accepts only np array
+	
+def create_middle_model(model, layer):
+	intermediate_layer_model = keras.Model(inputs=model.input,
+										   outputs=layer.output)
+	return intermediate_layer_model(data)
+	
+def info():
+	global layer
+	layer = model.layers[0]
+	sum_layer(layer)
+
+def classify_point_2d(x, b):	#recreate model if this function has been changed.
+	if(x>b):
+		return 0.1
+	return -0.1
+	
+def generate_train_data(b, n):		#train data x, lable y
+	X = []
+	Y = []
+	for i in range(n):
+		x = random.uniform(-1, 1)
+		y = classify_point_2d(x, b)
+		X.append(x)
+		Y.append(y)
+	return X, Y
+	
+def getY(x, w, b):
+	return (x*w)+b
+	
+def train(b, n, verbose = 0):
+	x, y = generate_train_data(b, n)
+	if verbose :
+		print(x, y)
+	history = model.fit(x, y, epochs=1, verbose=0)	#x input data, y target data
+	sum_layer(layer)
+	get_value(0)
+	
+	w, b = (layer.weights[0].numpy()[0], layer.weights[1].numpy()[0])
+
+	plt.scatter(x, y, marker='o', color="red")
+	plt.plot([-2, 2], [getY(-2, w, b), getY(2, w, b)])
+	plt.show()
